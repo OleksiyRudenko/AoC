0292a8156e7ff9ccb60610f8afe8a61c748f7717
OleksiyRudenko <oleksiy.rudenko@gmail.com>
2019-12-19 10:11:02 +0200

    2019/18: Move 18a and 18b to DISCARDED

--- 2019/DISCARD/i18a.js
+++ 2019/DISCARD/i18a.js
@@ -1,6 +1,6 @@
-const testSuite = require("./test-suite");
-const T = require("./tools");
-const mainInput = require("./i18--input");
+const testSuite = require("../TBD/test-suite");
+const T = require("../TBD/tools");
+const mainInput = require("../TBD/i18--input");
 
 console.log('====================== ABANDONED, WRONG APPROACH ====================');
 
